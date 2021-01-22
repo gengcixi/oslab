c51e0340bfed030fcdbd60a19d2358143122fa42
Cixi Geng <cixi.geng1@unisoc.com>
2021-01-22 10:47:44 +0800

    add basic build.sh

---
+++ build.sh
@@ -0,0 +1,13 @@
+#########################################################################
+# File Name: build.sh
+# Author: Cixi.Geng
+# mail: cixi.geng@unisoc.com
+# Created Time: Fri Jan 22 10:44:20 2021
+#########################################################################
+#!/bin/bash
+
+source ./func
+
+echo_info "start build.sh"
+echo_pass "build all is passed"
+echo_info "finish build.sh"
