f5fb620d40e236bc4217a8ad2f10ddfca41e2520
Cixi Geng <cixi.geng1@unisoc.com>
2021-02-01 17:58:08 +0800

    add .blueocean.groovy

---
+++ .blueocean.groovy
@@ -0,0 +1,25 @@
+#!groovy
+#Jenkinsfile (Declarative Pipeline)
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Building..'
+                echo "Running ${env.BUILD_ID} on ${env.JENKINS_URL}"
+                script: 'verify.sh'
+            }
+        }
+        stage('Test') {
+            steps {
+                echo 'Testing..'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploying....'
+            }
+        }
+    }
+}
